f93c56df4ec979f96d7acd272bf784c8b9a6d4f2
DebabrataBanik <debabratabanik02@gmail.com>
2024-12-23 17:37:38 +0530

    add header compo

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import trollface from '../../public/troll-face.png'
+
+function Header() {
+  return (
+    <header>
+      <img src={trollface} />
+      <h1>Meme Generator</h1>
+    </header>
+  )
+}
+
+export default Header
