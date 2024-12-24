f6be5a4e6d372d43bd09c047c73fc28aa2804509
DebabrataBanik <debabratabanik02@gmail.com>
2024-12-24 17:03:07 +0530

    add all functionalities
    fetch meme image with useEffect, randomly render meme img on change

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,69 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Meme from './Meme'
 
 function Main() {
 
   const [meme, setMeme] = useState({
-    topText: 'one does not simply',
-    bottomText: 'walk into mordor',
+    topText: 'top text',
+    bottomText: 'bottom text',
     imgUrl: 'http://i.imgflip.com/1bij.jpg'
   })
 
+  const [memeArr, setMemeArr] = useState([])
+
+  const handleChange = e => {
+    const { value, name } = e.currentTarget
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      [name]: value // this js feature is called Computed property names
+    }))
+  }
+
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then(res => res.json())
+      .then(data => setMemeArr(data.data.memes))
+  }, [])
+
+  const changeMemeImg = () => {
+    const rdnIdx = Math.floor(Math.random() * memeArr.length)
+    const newMemeImg = memeArr[rdnIdx].url
+    console.log(newMemeImg)
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      imgUrl: memeArr[rdnIdx].url
+    }))
+  }
+
   return (
     <main className='main'>
-      <form className='form'>
+      <section className='form'>
         <div className='left'>
           <label htmlFor="top">Top text</label>
-          <input type="text" id="top" name='top-text' placeholder='Shut up' />
+
+          <input
+            type="text"
+            id="top"
+            name='topText'
+            placeholder='Shut up'
+            value={meme.topText}
+            onChange={handleChange}
+          />
         </div>
         <div className='right'>
           <label htmlFor="bottom">Bottom text</label>
-          <input type="text" id="bottom" name='bottom-text' placeholder='And take my money' />
+          <input
+            type="text"
+            id="bottom"
+            name='bottomText'
+            placeholder='And take my money'
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
         </div>
-        <button className='submit'>Get a new meme image</button>
+        <button onClick={changeMemeImg} className='submit'>Get a new meme image</button>
 
-      </form>
+      </section>
 
       <Meme {...meme} />
     </main>
